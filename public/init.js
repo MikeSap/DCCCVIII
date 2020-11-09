34070b134179f55aabbd278fb481bd306ccf85e6
tjbigelow <tjbigelow@att.net>
2020-11-09 16:49:15 -0600

    added bank selector

--- public/init.js
+++ public/init.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", main())
 
 function main(){
     fetchSounds(1)
+    fetchBanks()
 }
 
 function fetchSounds(bankId){
@@ -17,3 +18,23 @@ function initializeBank(bank){
         padArray[index].innerText = `${sound.name}`
     })
 }
+
+function  fetchBanks(){
+    fetch('http://localhost:3000/banks')
+    .then(resp => resp.json())
+    .then(bankSelector)
+}
+
+function bankSelector(banks){
+    const select = document.getElementById('bank-select')
+    banks.forEach(bank => {
+        const option = document.createElement('option')
+        option.innerText = bank.name
+        option.dataset.id = bank.id
+        select.append(option)
+    })
+    select.addEventListener('change', e => {
+        fetchSounds(e.target.options[e.target.selectedIndex].dataset.id)
+    })
+}
+
