aa115dbe5823daacc7c65900a8dcb8fb22456082
tjbigelow <tjbigelow@att.net>
2020-11-10 09:45:51 -0600

    added Clear Song functionality to set a blank slate before loading

--- public/load-song.js
+++ public/load-song.js
@@ -1,4 +1,5 @@
 function loadSong(id){
+    clearSong()
     fetch(`http://localhost:3000/songs/${id}`)
     .then(resp => resp.json())
     .then(json => json.tracks.forEach((track, index) => {
@@ -14,4 +15,11 @@ function loadTrack(track, index){
         const position = trackNodes.find(pos => pos.dataset.position == ts.position)
         position.options.selectedIndex = ts.sound.sound_id
     })
+}
+
+function clearSong(){
+    for(let i = 0; i < 8; i++){
+        const trackNodes = Array.from(document.querySelectorAll('.track')[i].children)
+        trackNodes.forEach(pos => pos.selectedIndex = 0)
+    }
 }
