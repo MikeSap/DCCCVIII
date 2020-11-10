37e7eaf3671a262f2f31ef99e1746b0afeea4d6e
mike <mikesapienzaaudio@gmail.com>
2020-11-09 18:00:00 -0600

    form in prog

---
+++ public/save-song.js
@@ -0,0 +1,6 @@
+let songForm = document.querySelector('.song-form')
+
+songForm.addEventListener('submit', (e) => {
+    let sequencer = document.querySelector('#sequencer-container')
+    debugger
+})
