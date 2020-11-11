5def07c7143d9d88d00cade41c479d6440cc0c73
thesebonesalone <duikhead2@gmail.com>
2020-11-10 18:07:44 -0600

    minor styling changes

--- public/index.js
+++ public/index.js
@@ -1,169 +1,161 @@
 
-    let BPM = 120
-    let currentSoundId = 1
-    let position = 0
-    let bpmInput = document.getElementById("BPM-input")
-    const beatPad = document.querySelector('.beat-pad-container')
-    function playSound(node){
-        //debugger
-        let new_audio = node.cloneNode()
-        new_audio.play()
-    }
-    beatPad.addEventListener('mousedown', (e) => {
-    
+let BPM = 120
+let currentSoundId = 1
+let position = 0
+let bpmInput = document.getElementById("BPM-input")
+const beatPad = document.querySelector('.beat-pad-container')
+function playSound(node){
+    let new_audio = node.cloneNode()
+    new_audio.play()
+}
+beatPad.addEventListener('mousedown', (e) => {
 
-    if (e.target.className == "key-not-pressed"){
-        let button = e.target
-        //debugger
+
+if (e.target.className == "key-not-pressed"){
+    let button = e.target
+    let node = document.createElement('audio') 
+    node.setAttribute(`src`,`${sampleArray[parseInt(e.target.dataset.position)].src}`)
+    node.setAttribute('preload','auto')
+    playSound(node)
+    currentSoundId = parseInt(e.target.dataset.position)
+    button.className = "key-pressed"
+    setTimeout(function(){
+        button.className = "key-not-pressed"}, 1000)
+}    
+})
+
+beatPad.addEventListener('mouseup', (e) => {
+    if (e.target.className == "key-pressed"){
+        let button = e.target       
+    button.className = "key-not-pressed"
+    }    
+})
+
+document.onkeydown = (e) => {
+    if (e.code.includes("Numpad")){
+        let pad = document.querySelector(`#pad-${e.key}`)
 
         let node = document.createElement('audio') 
-        node.setAttribute(`src`,`${sampleArray[parseInt(e.target.dataset.position)].src}`)
+        node.setAttribute(`src`,`${sampleArray[parseInt(pad.dataset.position)].src}`)
+        currentSoundId = parseInt(pad.dataset.position)
         node.setAttribute('preload','auto')
         playSound(node)
-        currentSoundId = parseInt(e.target.dataset.position)
-        button.className = "key-pressed"
-        setTimeout(function(){
-            button.className = "key-not-pressed"}, 1000)
-    }    
-    })
-
-    beatPad.addEventListener('mouseup', (e) => {
-        if (e.target.className == "key-pressed"){
-            let button = e.target       
-        button.className = "key-not-pressed"
-        }    
-    })
-
-    document.onkeydown = (e) => {
-        if (e.code.includes("Numpad")){
-            let pad = document.querySelector(`#pad-${e.key}`)
-
-            let node = document.createElement('audio') 
-            node.setAttribute(`src`,`${sampleArray[parseInt(pad.dataset.position)].src}`)
-            currentSoundId = parseInt(pad.dataset.position)
-            node.setAttribute('preload','auto')
-            playSound(node)
-            if (pad.className == "key-not-pressed"){
-                    pad.className = "key-pressed"  
-            }
-        }
-    }  
-
-    document.onkeyup = (e) => {
-        let pad = document.querySelector(`#pad-${e.key}`)
-        if (e.code.includes("Numpad")){
-            if (pad.className == "key-pressed"){
-                    pad.className = "key-not-pressed"  
-            }
+        if (pad.className == "key-not-pressed"){
+                pad.className = "key-pressed"  
         }
     }
+}  
 
-    bpmInput.addEventListener("input", function(e){
-        console.log(e.target.value)
-        BPM = e.target.value
-        if (play === true){
-            clearInterval(beat)
-            beat = setInterval(function(){document.dispatchEvent(oneBeat)},30000/BPM)
-        }
-    })
-    
-    
-
-
-    let playbtn = document.getElementById("play")
-    let stopbtn = document.getElementById("stop")
-    let play = false
-    
-    let oneBeat = new CustomEvent('oneBeat')
-
-    function setUpSequencer(){
-        contain = document.getElementById("sequencer-container")
-        contain.innerHTML = ' '
-        for (let y = 0; y < 8; y += 1){
-            let p = document.createElement("div")
-            p.className = 'track'
-            
-            contain.append(p)
-            p.dataset.trackId = y+1
-            for (let x = 0; x < 16; x += 1){
-                let select = document.createElement("div")
-                select.setAttribute("class","row sequence-input inert unclicked")
-                select.dataset.soundInfo = ' '
-                select.dataset.soundId = 0
-                select.dataset.position = x
-                select.addEventListener("click",function(e){
-
-                    if (e.target.dataset.soundInfo != sampleArray[currentSoundId].src)
-                    {
-                        console.log(e)
-                        e.target.innerText = sampleArray[currentSoundId].name
-                        e.target.dataset.soundInfo = sampleArray[currentSoundId].src
-                        
-                        e.target.dataset.soundId = sampleArray[currentSoundId].id
-                        let status = e.target.getAttribute("class").split(" ")
-                        console.log(status)
-                        e.target.setAttribute("class",`row ${status[1]} ${status[2]} clicked`)
-                    } else {
-                        e.target.innerText = ' '
-                        e.target.dataset.soundInfo = ' '
-                        e.target.dataset.soundId = 0
-                        let status = e.target.getAttribute("class").split(" ")
-                        e.target.setAttribute("class",`row ${status[1]} ${status[2]} unclicked`)
-                    }
-
-                })
-                p.append(select)
-            }
+document.onkeyup = (e) => {
+    let pad = document.querySelector(`#pad-${e.key}`)
+    if (e.code.includes("Numpad")){
+        if (pad.className == "key-pressed"){
+                pad.className = "key-not-pressed"  
         }
     }
+}
 
-    playbtn.addEventListener("click",function(){
-        if (play === false)
-        {
-            play = true
-            console.log("working")
-            beat = setInterval(function(){document.dispatchEvent(oneBeat)},30000/BPM)
+bpmInput.addEventListener("input", function(e){
+    console.log(e.target.value)
+    BPM = e.target.value
+    if (play === true){
+        clearInterval(beat)
+        beat = setInterval(function(){document.dispatchEvent(oneBeat)},30000/BPM)
+    }
+})
+
+let playbtn = document.getElementById("play")
+let stopbtn = document.getElementById("stop")
+let play = false
+let oneBeat = new CustomEvent('oneBeat')
+
+function setUpSequencer(){
+    contain = document.getElementById("sequencer-container")
+    contain.innerHTML = ' '
+    for (let y = 0; y < 8; y += 1){
+        let p = document.createElement("div")
+        p.className = 'track'
+        
+        contain.append(p)
+        p.dataset.trackId = y+1
+        for (let x = 0; x < 16; x += 1){
+            let select = document.createElement("div")
+            select.setAttribute("class","row sequence-input inert unclicked")
+            select.dataset.soundInfo = ' '
+            select.dataset.soundId = 0
+            select.dataset.position = x
+            select.addEventListener("click",function(e){
+
+                if (e.target.dataset.soundInfo != sampleArray[currentSoundId].src)
+                {
+                    console.log(e)
+                    e.target.innerText = sampleArray[currentSoundId].name
+                    e.target.dataset.soundInfo = sampleArray[currentSoundId].src
+                    
+                    e.target.dataset.soundId = sampleArray[currentSoundId].id
+                    let status = e.target.getAttribute("class").split(" ")
+                    console.log(status)
+                    e.target.setAttribute("class",`row ${status[1]} ${status[2]} clicked`)
+                } else {
+                    e.target.innerText = ' '
+                    e.target.dataset.soundInfo = ' '
+                    e.target.dataset.soundId = 0
+                    let status = e.target.getAttribute("class").split(" ")
+                    e.target.setAttribute("class",`row ${status[1]} ${status[2]} unclicked`)
+                }
+
+            })
+            p.append(select)
         }
-    })
+    }
+}
 
-    stopbtn.addEventListener("click",function(){
-        if (play === true)
-        {
-            play = false
-            position = 0
-            console.log("Stopped")
-            clearInterval(beat)
-        }
-    })
+playbtn.addEventListener("click",function(){
+    if (play === false)
+    {
+        play = true
+        console.log("working")
+        beat = setInterval(function(){document.dispatchEvent(oneBeat)},30000/BPM)
+    }
+})
 
-    document.addEventListener("oneBeat", function(){
-        let sounds = document.getElementsByClassName("sequence-input")
-        for (let i = 0; i < sounds.length; i += 1)
-        {
+stopbtn.addEventListener("click",function(){
+    if (play === true)
+    {
+        play = false
+        position = 0
+        console.log("Stopped")
+        clearInterval(beat)
+    }
+})
+
+document.addEventListener("oneBeat", function(){
+    let sounds = document.getElementsByClassName("sequence-input")
+    for (let i = 0; i < sounds.length; i += 1)
+    {
+        let status = sounds[i].getAttribute("class").split(" ")
+        console.log(status)
+        sounds[i].setAttribute("class",`row ${status[1]} active ${status[3]}`)
+        if (parseInt(sounds[i].dataset.position) === position){
+            pos = sounds[i].dataset.soundInfo
+            if (pos != " "){
+                let node = document.createElement('audio') 
+                node.setAttribute(`src`,`${pos}`)
+                node.setAttribute('preload','auto')
+                playSound(node)
+                console.log(node)
+            }
+        } else {
+
             let status = sounds[i].getAttribute("class").split(" ")
             console.log(status)
-            sounds[i].setAttribute("class",`row ${status[1]} active ${status[3]}`)
-            if (parseInt(sounds[i].dataset.position) === position){
-                pos = sounds[i].dataset.soundInfo
-                if (pos != " "){
-                    let node = document.createElement('audio') 
-                    node.setAttribute(`src`,`${pos}`)
-                    node.setAttribute('preload','auto')
-                    playSound(node)
-                    console.log(node)
-                    //sampleArray[pos].play()
-                }
-            } else {
-
-                let status = sounds[i].getAttribute("class").split(" ")
-                console.log(status)
-                sounds[i].setAttribute("class",`row ${status[1]} inert ${status[3]}`)
-            }
+            sounds[i].setAttribute("class",`row ${status[1]} inert ${status[3]}`)
         }
-        position += 1
-        if (position === 16)
-        {
-            position = 0
-        }
-    })
-    setUpSequencer()
+    }
+    position += 1
+    if (position === 16)
+    {
+        position = 0
+    }
+})
+setUpSequencer()
--- public/init.js
+++ public/init.js
@@ -1,64 +1,64 @@
 document.addEventListener("DOMContentLoaded", main())
 
-function main(){
-    fetchSounds(1)
-    fetchBanks()
-    fetchSongs()
-}
+    function main(){
+        fetchSounds(1)
+        fetchBanks()
+        fetchSongs()
+    }
 
-function fetchSounds(bankId){
-    fetch(`http://localhost:3000/banks/${bankId}`)
-    .then(resp => resp.json())
-    .then(initializeBank)
-}
+    function fetchSounds(bankId){
+        fetch(`http://localhost:3000/banks/${bankId}`)
+        .then(resp => resp.json())
+        .then(initializeBank)
+    }
 
-function initializeBank(bank){
-    bank.sounds.forEach((sound, index) => {
-        let new_sound = {}
-        new_sound.src = `${sound.location}`
-        new_sound.name = sound.name
-        new_sound.id = sound.id
-        sampleArray[index + 1] = new_sound
-        padArray[index].innerText = `${sound.name}`
-    })
-}
+    function initializeBank(bank){
+        bank.sounds.forEach((sound, index) => {
+            let new_sound = {}
+            new_sound.src = `${sound.location}`
+            new_sound.name = sound.name
+            new_sound.id = sound.id
+            sampleArray[index + 1] = new_sound
+            padArray[index].innerText = `${sound.name}`
+        })
+    }
 
-function  fetchBanks(){
-    fetch('http://localhost:3000/banks')
-    .then(resp => resp.json())
-    .then(bankSelector)
-}
+    function  fetchBanks(){
+        fetch('http://localhost:3000/banks')
+        .then(resp => resp.json())
+        .then(bankSelector)
+    }
 
-function bankSelector(banks){
-    const select = document.getElementById('bank-select')
-    banks.forEach(bank => {
-        const option = document.createElement('option')
-        option.innerText = bank.name
-        option.dataset.id = bank.id
-        select.append(option)
-    })
-    select.addEventListener('change', e => {
-        fetchSounds(e.target.options[e.target.selectedIndex].dataset.id)
-    })
-}
+    function bankSelector(banks){
+        const select = document.getElementById('bank-select')
+        banks.forEach(bank => {
+            const option = document.createElement('option')
+            option.innerText = bank.name
+            option.dataset.id = bank.id
+            select.append(option)
+        })
+        select.addEventListener('change', e => {
+            fetchSounds(e.target.options[e.target.selectedIndex].dataset.id)
+        })
+    }
 
-function fetchSongs(){
-    fetch('http://localhost:3000/songs')
-    .then(resp => resp.json())
-    .then(songSelector)
-}
+    function fetchSongs(){
+        fetch('http://localhost:3000/songs')
+        .then(resp => resp.json())
+        .then(songSelector)
+    }
 
-function songSelector(songs){
-    const select = document.getElementById('load-song')
-    songs.forEach(song => {
-        const option = document.createElement('option')
-        option.innerText = song.title
-        option.dataset.id = song.id
-        select.append(option)
-    })
-    select.addEventListener('change', e => {
-        loadSong(parseInt(e.target.options[e.target.selectedIndex].dataset.id))
-    })
+    function songSelector(songs){
+        const select = document.getElementById('load-song')
+        songs.forEach(song => {
+            const option = document.createElement('option')
+            option.innerText = song.title
+            option.dataset.id = song.id
+            select.append(option)
+        })
+        select.addEventListener('change', e => {
+            loadSong(parseInt(e.target.options[e.target.selectedIndex].dataset.id))
+        })
 
-}
+    }
 
